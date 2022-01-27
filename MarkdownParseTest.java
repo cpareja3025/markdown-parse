95c649db2b34218820455dff3eef32989eb19177
cpareja3025 <cpareja3025@gmail.com>
2022-01-26 16:38:00 -0800

    new functions for Junit testing

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,47 @@
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+
 import org.junit.*;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
     }
+
+    @Test
+    public void testFile() throws IOException{
+        Path filename = Path.of("test-file.md");
+        String contents = Files.readString(filename);
+        assertEquals(List.of("https://something.com", "some-page.html"),MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void break1() throws IOException{
+        Path filename = Path.of("break1.md");
+        String contents = Files.readString(filename);
+        assertEquals(List.of("https://something.com", "some-page.html"),MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void break2() throws IOException{
+        Path filename = Path.of("break2.md");
+        String contents = Files.readString(filename);
+        assertEquals(List.of("https://something().com"),MarkdownParse.getLinks(contents));
+    }
+    @Test
+    public void break3() throws IOException{
+        Path filename = Path.of("break3.md");
+        String contents = Files.readString(filename);
+        assertEquals(List.of(),MarkdownParse.getLinks(contents));
+    }
+
+    
 }
